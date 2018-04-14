7720a49413e692a737b061ac9cf93336602b5b88
NIKMC <nikmc10@mail.ru>
2018-04-14 13:43:57 +0300

    Init project bases on image recognition

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func photoTapped(_ sender: UIBarButtonItem) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
