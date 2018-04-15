4d7bd7ebf0044b9d681b1d5d85a6e6d1339e6bd0
NIKMC <nikmc10@mail.ru>
2018-04-15 13:55:41 +0300

    add workspace

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -14,6 +14,8 @@ import SwiftyJSON
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var descriptionFlower: UILabel!
+    private let wikipediaURL = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as? String
     @IBOutlet weak var imagePhoto: UIImageView!
     let imagePicker = UIImagePickerController()
     
@@ -30,12 +32,41 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             let classification = request.results?.first as? VNClassificationObservation
         
             self.navigationItem.title = classification?.identifier.capitalized
+            
+            self.getInfo(title: (classification?.identifier.capitalized)!)
         }
         
         let handler = VNImageRequestHandler(ciImage: image, options: [:])
         do{
             try handler.perform([request])
         } catch {
+            
+        }
+    }
+    
+    func getInfo(title: String) {
+        let parameter : [String: String] = [
+            "format":"json",
+            "action":"query",
+            "prop":"extracts",
+            "exintro":"",
+            "explaintext":"",
+            "titles":title,
+            "indexpageids":"",
+            "redirects":"1",
+        ]
+        Alamofire.request(wikipediaURL!, method: .get, parameters: parameter).responseJSON { (response) in
+            guard response.result.isSuccess else {
+                print("Error while fetching response: \(String(describing: response.result.error))")
+                return
+            }
+            print(JSON(response.result.value!))
+            let flowerJSON : JSON = JSON(response.result.value!)
+            
+            let pageId = flowerJSON["query"]["pageids"][0].stringValue
+
+            let flowerDescription = flowerJSON["query"]["pages"][pageId]["extract"].stringValue
+            self.descriptionFlower.text = flowerDescription
             
         }
     }
