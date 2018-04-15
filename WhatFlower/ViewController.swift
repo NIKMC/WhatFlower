0289511f9b36350773711ea380967031f1722889
NIKMC <nikmc10@mail.ru>
2018-04-15 12:43:30 +0300

    add photo recognition & import libs

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -7,20 +7,57 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imagePhoto: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
     @IBAction func photoTapped(_ sender: UIBarButtonItem) {
-        
+        present(imagePicker, animated: true, completion: nil)
     }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("can't import flowerClassifier model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            let classification = request.results?.first as? VNClassificationObservation
+        
+            self.navigationItem.title = classification?.identifier.capitalized
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image, options: [:])
+        do{
+            try handler.perform([request])
+        } catch {
+            
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            guard let convertedCIImage = CIImage(image: userPickedImage) else {
+                fatalError("Cannot convert to ciImage")
+            }
+            detect(image: convertedCIImage)
+            
+            imagePhoto.image = userPickedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .camera
+        
     }
 
 
